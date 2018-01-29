f42de8a38e0b0e34b6c6f77d817c614046fffba9
Imelstorm <const_ant_in@virgilsecurity.com>
2018-01-29 16:47:32 +0200

    Return distclass option

--- virgil_crypto/hashes/fingerprint.py
+++ virgil_crypto/hashes/fingerprint.py
@@ -32,37 +32,48 @@
 # IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-import unittest
-
-from virgil_crypto.virgil_crypto_python import VirgilCipher
-from virgil_crypto.virgil_crypto_python import VirgilKeyPair
+import virgil_crypto
 
 
-class VirgilCipherTest(unittest.TestCase):
-    def test_encrypts_and_decrypts_data(self):
-        raw_data = bytearray("test", "utf-8")
-        key_pair1 = VirgilKeyPair.generate(VirgilKeyPair.Type_FAST_EC_ED25519)
-        key_pair2 = VirgilKeyPair.generate(VirgilKeyPair.Type_FAST_EC_ED25519)
-        cipher = VirgilCipher()
-        cipher.addKeyRecipient(bytearray("1", "utf-8"), key_pair1.publicKey())
-        cipher.addKeyRecipient(bytearray("2", "utf-8"), key_pair2.publicKey())
-        encrypted_data = cipher.encrypt(raw_data)
-        cipher = VirgilCipher()
-        decrypted_data1 = cipher.decryptWithKey(
-            encrypted_data,
-            bytearray("1", "utf-8"),
-            key_pair1.privateKey()
-        )
-        self.assertEqual(
-            raw_data,
-            bytearray(decrypted_data1)
-        )
-        decrypted_data2 = cipher.decryptWithKey(
-            encrypted_data,
-            bytearray("2", "utf-8"),
-            key_pair2.privateKey()
-        )
-        self.assertEqual(
-            raw_data,
-            bytearray(decrypted_data2)
-        )
+class Fingerprint(object):
+    """Fingerprint container class.
+
+    Class provides methods for importing and exporting fingerprints.
+    """
+    def __init__(self, fingerprint_data):
+        self._fingerprint_data = fingerprint_data
+
+    @classmethod
+    def from_hex(cls, fingerprint_hex):
+        # type: (str) -> Fingerprint
+        """Creates new Fingerprint from hex.
+
+        Args:
+            fingerprint_hex: hex string of the fingerprint.
+
+        Returns:
+            Imported Fingerprint.
+        """
+        data = virgil_crypto.VirgilByteArrayUtils.hexToBytes(fingerprint_hex)
+        return cls(data)
+
+    @property
+    def value(self):
+        # type: () -> Tuple[*int]
+        """Raw fingerprint value.
+
+        Returns:
+            Fingerprint bytes.
+        """
+        return self._fingerprint_data
+
+    @property
+    def to_hex(self):
+        # type: () -> str
+        """Fingerprint data in hexadecimal.
+
+        Returns:
+            Hexademical fingerprint representation.
+        """
+        hex_data = virgil_crypto.VirgilByteArrayUtils.bytesToHex(self.value)
+        return hex_data
--- virgil_crypto/tests/hashes/fingerprint_test.py
+++ virgil_crypto/tests/hashes/fingerprint_test.py
@@ -34,35 +34,22 @@
 
 import unittest
 
-from virgil_crypto.virgil_crypto_python import VirgilCipher
-from virgil_crypto.virgil_crypto_python import VirgilKeyPair
+from virgil_crypto import VirgilCrypto
+from virgil_crypto.hashes import Fingerprint
 
 
-class VirgilCipherTest(unittest.TestCase):
-    def test_encrypts_and_decrypts_data(self):
-        raw_data = bytearray("test", "utf-8")
-        key_pair1 = VirgilKeyPair.generate(VirgilKeyPair.Type_FAST_EC_ED25519)
-        key_pair2 = VirgilKeyPair.generate(VirgilKeyPair.Type_FAST_EC_ED25519)
-        cipher = VirgilCipher()
-        cipher.addKeyRecipient(bytearray("1", "utf-8"), key_pair1.publicKey())
-        cipher.addKeyRecipient(bytearray("2", "utf-8"), key_pair2.publicKey())
-        encrypted_data = cipher.encrypt(raw_data)
-        cipher = VirgilCipher()
-        decrypted_data1 = cipher.decryptWithKey(
-            encrypted_data,
-            bytearray("1", "utf-8"),
-            key_pair1.privateKey()
-        )
+class FingerptintTest(unittest.TestCase):
+    @property
+    def _crypto(self):
+        return VirgilCrypto()
+
+    def test_from_to_hex(self):
+        data = bytearray([1, 2, 3])
+        fingerprint = self._crypto.calculate_fingerprint(data)
+        hex_string = fingerprint.to_hex
+        rebuilt_fingerprint = Fingerprint.from_hex(hex_string)
         self.assertEqual(
-            raw_data,
-            bytearray(decrypted_data1)
+            fingerprint.value,
+            rebuilt_fingerprint.value
         )
-        decrypted_data2 = cipher.decryptWithKey(
-            encrypted_data,
-            bytearray("2", "utf-8"),
-            key_pair2.privateKey()
-        )
-        self.assertEqual(
-            raw_data,
-            bytearray(decrypted_data2)
-        )
+
