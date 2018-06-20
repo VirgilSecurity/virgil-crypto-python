2d5d5a0f3e793ca26e1eac03f2a17a9f00ded2a6
Imelstorm <const_ant_in@virgilsecurity.com>
2018-06-20 12:49:08 +0300

    Fix import of private key exporter

--- virgil_crypto/keys/__init__.py
+++ virgil_crypto/keys/__init__.py
@@ -36,3 +36,4 @@ from .key_pair import KeyPair
 from .key_pair_type import KeyPairType
 from .private_key import PrivateKey
 from .public_key import PublicKey
+from .private_key_exporter import PrivateKeyExporter
