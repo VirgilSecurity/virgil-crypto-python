0baf7a97c42f39d46730a8d0744d18206a751749
Imelstorm <const_ant_in@virgilsecurity.com>
2018-06-27 16:09:40 +0300

    Fix description tag

--- setup.py
+++ setup.py
@@ -36,6 +36,10 @@ setup(
     license="BSD",
     include_package_data=True,
     zip_safe=False,
+    description="""
+    Virgil Security provides a set of APIs for adding security to any application. In a few simple steps you can encrypt communication, securely store data, provide passwordless login, and ensure data integrity.
+    Virgil Python Crypto Library is a high-level cryptographic library that allows you to perform all necessary operations for secure storing and transferring data and everything required to become HIPAA and GDPR compliant.
+    """,
     long_description="""
     Virgil Security provides a set of APIs for adding security to any application. In a few simple steps you can encrypt communication, securely store data, provide passwordless login, and ensure data integrity.
     Virgil Python Crypto Library is a high-level cryptographic library that allows you to perform all necessary operations for secure storing and transferring data and everything required to become HIPAA and GDPR compliant.
